726f1d3c28dd126e1a314ef3aac26e9a47af13da
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-01-06 23:26:01 -0300

    atv

---
+++ script.js
@@ -0,0 +1,24 @@
+function checkLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "dyrson1993" && password === "26121993") {
+        openModal();
+    } else {
+        alert("Credenciais incorretas. Tente novamente.");
+    }
+}
+
+function openModal() {
+    var modal = document.getElementById("welcomeModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("welcomeModal");
+    modal.style.display = "none";
+}
+
+function redirectToWelcomePage() {
+    window.location.href = "../paginas/pag_boas_vindas/index.html";
+}
